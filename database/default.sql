403f4168e260b04ac53e79635c7a394b2d5b67b2
Christian <christian.moseby@gmail.com>
2017-10-23 14:26:51 -0500

    Insert statements

---
+++ database/default.sql
@@ -0,0 +1,16 @@
+INSERT INTO Activity (ActivityId, ActivityName) Values (1, "Romantic");
+INSERT INTO Activity (ActivityId, ActivityName) Values (2, "Outdoor Adventure");
+INSERT INTO Activity (ActivityId, ActivityName) Values (3, "Laidback");
+
+INSERT INTO AgeGroup (AgeGroupId, AgeGroupName) Values (1, "Under 21");
+INSERT INTO AgeGroup (AgeGroupId, AgeGroupName) Values (2, "Over 21");
+
+INSERT INTO RelationshipStatus (RelationshipStatusId, RelationshipStatusName) Values (1, "Newly Dating");
+INSERT INTO RelationshipStatus (RelationshipStatusId, RelationshipStatusName) Values (2, "It's Serious");
+INSERT INTO RelationshipStatus (RelationshipStatusId, RelationshipStatusName) Values (3, "We're Married");
+
+INSERT INTO Price (PriceId, PriceName) Values (1, "Free");
+INSERT INTO Price (PriceId, PriceName) Values (2, "Budget");
+INSERT INTO Price (PriceId, PriceName) Values (3, "Expensive");
+
+
