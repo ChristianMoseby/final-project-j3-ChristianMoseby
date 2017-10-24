500ba4389367803a201cbbfbb36f0596daacd3ba
Christian <christian.moseby@gmail.com>
2017-10-23 22:59:20 -0500

    Insert statements-errors

--- database/default.sql
+++ database/default.sql
@@ -1,9 +1,10 @@
-INSERT INTO Activity (ActivityId, ActivityName) Values (1, "Romantic");
-INSERT INTO Activity (ActivityId, ActivityName) Values (2, "Outdoor Adventure");
-INSERT INTO Activity (ActivityId, ActivityName) Values (3, "Laidback");
+INSERT INTO Activity (ActivityId, ActivityTypeId) Values ()
 
-INSERT INTO AgeGroup (AgeGroupId, AgeGroupName) Values (1, "Under 21");
-INSERT INTO AgeGroup (AgeGroupId, AgeGroupName) Values (2, "Over 21");
+
+INSERT INTO ActivityType (ActivityTypeId, ActiviyTypeName) Values (1, "Romantic");
+INSERT INTO ActivityType (ActivityTypeId, ActiviyTypeName) Values (2, "Adventurous");
+INSERT INTO ActivityType (ActivityTypeId, ActiviyTypeName) Values (3, "Casual");
+
 
 INSERT INTO RelationshipStatus (RelationshipStatusId, RelationshipStatusName) Values (1, "Newly Dating");
 INSERT INTO RelationshipStatus (RelationshipStatusId, RelationshipStatusName) Values (2, "It's Serious");
@@ -12,5 +13,16 @@ INSERT INTO RelationshipStatus (RelationshipStatusId, RelationshipStatusName) Va
 INSERT INTO Price (PriceId, PriceName) Values (1, "Free");
 INSERT INTO Price (PriceId, PriceName) Values (2, "Budget");
 INSERT INTO Price (PriceId, PriceName) Values (3, "Expensive");
+
+INSERT INTO TimeOfDay (TimeOfDayId, TimeOfDayName) Values (1, "Day");
+INSERT INTO TimeOfDay (TimeOfDayId, TimeOfDayName) Values (2, "Night");
+INSERT INTO TimeOfDay (TimeOfDayId, TimeOfDayName) Values (3, "Any");
+
+INSERT INTO TimeOfYear (TimeOfYearId, TimeOfYearName) Values (1, "Spring/Summer");
+INSERT INTO TimeOfYear (TimeOfYearId, TimeOfYearName) Values (1, "Autumn/Winter");
+
+
+
+
 
 
